9857c569b0b8cccd6e58b933649ce8c74e2fe84a
Lincoln Bryant <lincolnb@uchicago.edu>
2018-05-24 15:30:37 -0500

    fixed compat with older qemu maybe

--- launch.sh
+++ launch.sh
@@ -6,20 +6,25 @@
 sudo systemctl stop vboxdrv
 sudo modprobe -r vboxdrv vboxnetflt vboxpci vboxnetadp
 
-sudo virsh destroy container-linux2
-sudo virsh undefine container-linux2
+sudo virsh destroy slate
+sudo virsh undefine slate
 
-sudo rm -f container-linux2.qcow
-sudo qemu-img create -f qcow2 -b coreos_production_qemu_image.img container-linux2.qcow2
+sudo rm -f slate.qcow
+sudo qemu-img create -f qcow2 -b coreos_production_qemu_image.img slate.qcow2
 
 sudo virt-install --connect qemu:///system \
              --import \
-             --name container-linux2 \
+             --name slate \
              --ram 1024 --vcpus 1 \
              --os-type=linux \
              --os-variant=virtio26 \
-             --disk path=container-linux2.qcow2,format=qcow2,bus=virtio \
+             --disk path=slate.qcow2,format=qcow2,bus=virtio \
              --vnc --noautoconsole \
              --qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=/home/lincolnb/slate-libvirt/config.ign"
+             --print-xml > domain.xml
 
-sudo virsh console container-linux2
+sed -ie 's|type="kvm"|type="kvm" xmlns:qemu="http://libvirt.org/schemas/domain/qemu/1.0"|' domain.xml
+sed -i "/<\/devices>/a <qemu:commandline>\n  <qemu:arg value='-fw_cfg'/>\n  <qemu:arg value='name=opt/com.coreos/config,file=config.ign'/>\n</qemu:commandline>" domain.xml
+
+
+sudo virsh console slate
