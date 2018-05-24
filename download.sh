3fbab65020cbbddf5eb0ea4a5173bdbec503d355
Lincoln Bryant <lincolnb@uchicago.edu>
2018-05-24 15:08:30 -0500

    make ct executable

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -L https://github.com/coreos/container-linux-config-transpiler/releases/download/v0.9.0/ct-v0.9.0-x86_64-unknown-linux-gnu > ct
+chmod +x ct
 wget https://beta.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2
 bunzip2 coreos_production_qemu_image.img.bz2
